a25defe85693461b9734f7c1e1f5a95a470c0945
chicorasia <chicorasia@gmail.com>
2021-05-10 14:56:52 -0300

    Adicionado layout tela AjustesFragment

--- src/main/java/br/com/chicorialabs/picpayclonekt/ui/ajustes/AjustesFragment.kt
+++ src/main/java/br/com/chicorialabs/picpayclonekt/ui/ajustes/AjustesFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import br.com.chicorialabs.picpayclonekt.databinding.FragmentAjustesBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -21,10 +19,7 @@ class AjustesFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAjustesBinding.inflate(layoutInflater, container, false)
-        val textView: TextView = binding.textNotifications
-        ajustesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return binding.root
     }
 }
