e9624b543e683f13f76843337347c25289d478c7
chicorasia <chicorasia@gmail.com>
2021-05-18 16:29:07 -0300

    Adicionado okhttpclient com interceptors, classe Token

---
+++ src/main/java/br/com/chicorialabs/picpayclonekt/data/Token.kt
@@ -0,0 +1,6 @@
+package br.com.chicorialabs.picpayclonekt.data
+
+data class Token(
+    val tipo: String,
+    val token: String
+)
--- src/main/java/br/com/chicorialabs/picpayclonekt/service/RetrofitService.kt
+++ src/main/java/br/com/chicorialabs/picpayclonekt/service/RetrofitService.kt
@@ -1,16 +1,33 @@
 package br.com.chicorialabs.picpayclonekt.service
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 private val URL = "http://192.168.0.187:8080/"
 
 object RetrofitService {
 
     val instance = Retrofit.Builder()
-            .baseUrl(URL)
-            .addConverterFactory(GsonConverterFactory.create())
+        .client(criarHttpClient())
+        .baseUrl(URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private fun criarHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addNetworkInterceptor(httpLoggingInterceptor)
+            .connectTimeout(1, TimeUnit.SECONDS)
+            .readTimeout(40, TimeUnit.SECONDS)
+            .writeTimeout(40, TimeUnit.SECONDS)
             .build()
+    }
 
     inline fun <reified T> criarService() = instance.create(T::class.java)
 }
